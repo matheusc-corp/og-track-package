3d1fce3342ccb2330db3a9b4aa76141d4694b3a6
Matheus Carvalho <matheus.carvalho.corp@gmail.com>
2025-06-25 15:46:00 +0900

    Feature: Finished the access into the track website, search using the track code e get the last status of the delivery

--- main.py
+++ main.py
@@ -9,9 +9,26 @@ O rastreio vai ser no site da AliExpress Selection Standard
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 import time
+from selenium.webdriver.support.wait import WebDriverWait
 
 driver = webdriver.Chrome()
-driver.get('https://www.google.com.br')
-time.sleep(2)
+driver.get('https://www.ordertracker.com/couriers/aliexpress')
+time.sleep(5)
+
+wait = WebDriverWait(driver, 15)
+searchInput = driver.find_element(By.TAG_NAME, 'input')
+
+searchInput.send_keys('CNBR00077223717')
+time.sleep(1)
+searchInput.submit()
+
+time.sleep(10)
+getStatus = driver.find_element(By.CLASS_NAME, 'status-content')
+
+print(getStatus.text)
+
+
+
 
